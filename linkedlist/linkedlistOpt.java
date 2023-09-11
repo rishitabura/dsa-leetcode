4024d25d2ef2bb00e8e39662257f1f31dbe1ef82
Rishita <burarishita@gmail.com>
2023-09-11 13:31:46 +0530

    practice

---
+++ linkedlist/linkedlistOpt.java
@@ -0,0 +1,19 @@
+package linkedlist;
+
+public class reverse {
+    class node{
+        node head;
+        int data;
+        node next;
+
+        node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        node n1 = new node();
+    }
+}
