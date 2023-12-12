a5c0c8310e3246cb4ae51b313593115c3e5aecc9
Rishita <burarishita@gmail.com>
2023-12-12 22:43:36 +0530

    day 3 problems

--- pointer.cpp
+++ pointer.cpp
@@ -4,28 +4,28 @@ using namespace std;
 
 int main()
 {
-    // int a = 10;
-    // int *aptr;
-    // aptr = &a;
+    int a = 10;
+    int *aptr;
+    aptr = &a;
 
-    // cout << a<< endl;
-    // cout << aptr << endl;
-    // cout << &a << endl;
-    // cout << *aptr<< endl;
+    cout << a<< endl;
+    cout << aptr << endl;
+    cout << &a << endl;
+    cout << *aptr<< endl;
 
-    // aptr++;
-    // cout << aptr<< endl;
+    aptr++;
+    cout << aptr<< endl;
 
 
-    // int arr[] = {10,20,30};
-    // cout << *arr << endl; 
+    int arr[] = {10,20,30};
+    cout << *arr << endl; 
 
-    // int *ptr = arr;
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     cout << *ptr << endl;
-    //     ptr++;
-    // }
+    int *ptr = arr;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << *ptr << endl;
+        ptr++;
+    }
 
 
     int b = 12;
