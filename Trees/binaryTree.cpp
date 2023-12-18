7147b0402a94f4c235bf8d861406e7a2ecf6b171
Rishita <burarishita@gmail.com>
2023-12-18 12:09:31 +0530

    day 9 trees

--- Trees/binaryTree.cpp
+++ Trees/binaryTree.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include<queue>
 using namespace std;
 
 /*
@@ -6,7 +7,7 @@ using namespace std;
 2. max nodes in a tree with height H = 2^H-1
 3. for N nodes, min height or levels = log2(N+1)
 */
-
+static int count = 0;
 class node
 {
 public:
@@ -76,26 +77,85 @@ int height(node *root)
     }
 }
 
-void leafNodes(node* root)
+void leafNodes(node *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    if (root->left == nullptr && root->right == nullptr)
+    {
+        cout << root->data << " ";
+    }
+    if (root->left != nullptr)
+    {
+        leafNodes(root->left);
+    }
+    if (root->right != nullptr)
+    {
+        leafNodes(root->right);
+    }
+}
+int countNode(node *root)
+{
+
+    if (root == nullptr)
+    {
+        return 0;
+    }
+
+    int l, r;
+    l = countNode(root->left);
+    r = countNode(root->right);
+
+    return l + r + 1;
+}
+
+void levelwiseDisplay(node* root)
 {
     if (root==nullptr)
     {
         return;
     }
-    if (root->left == nullptr && root->right==nullptr)
+    else
     {
-        cout << root->data << " "; 
-    }
-    if (root->left!=nullptr)
-    {
-        leafNodes(root->left);
-    }
-    if(root->right!=nullptr)
-    {
-        leafNodes(root->right);
+        queue<node*> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            node* temp = q.front();
+            cout << temp->data << " ";
+
+            if (temp->left!=nullptr)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right!=nullptr)
+            {
+                q.push(temp->right);
+            }
+            q.pop();       
+                 
+        }
     }
     
 }
+
+void mirrorImage(node* root)
+{
+    node *temp;
+    if (root!=nullptr)
+    {
+        mirrorImage(root->left);
+        mirrorImage(root->right);
+        temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+
+    }
+    
+}
+
 int main()
 {
     node *root = new node(1);
@@ -104,6 +164,7 @@ int main()
     root->left->left = new node(3);
     root->left->right = new node(6);
     root->left->right->left = new node(7);
+    root->right->left = new node(8);
     cout << "Preorder : ";
     preorder(root);
     cout << "\nInorder : ";
@@ -113,6 +174,14 @@ int main()
     cout << "\nHeight : " << height(root);
     cout << "\nLeaf nodes : ";
     leafNodes(root);
+    // int count = 0;
+    cout << "\nCount : " << countNode(root);
+    cout << "\nLevel wise display : ";
+    levelwiseDisplay(root);
+    cout << "\nMirror image preeoder : ";
+    mirrorImage(root);
+    preorder(root);
+
 
     return 0;
 }
