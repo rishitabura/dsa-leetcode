db76707a9fd4b817ad5d729bfe89d3875fb596f2
Rishita <burarishita@gmail.com>
2023-12-22 18:47:35 +0530

    day 12 problems

--- leetcode_questions/longestsubarrayK.cpp
+++ leetcode_questions/longestsubarrayK.cpp
@@ -3,41 +3,42 @@
 using namespace std;
 
 int longestSubarrayWithSumK(vector<int> a, long long k) {
-     int n = a.size();
+    int n = a.size();
     int left = 0;
     int right = 0;
     int sum = a[0];
     int maxlen = 0;
-    int start, end;
     while(right<n)
     {
 
-        while(sum>k && left<=right)
-        {
-            sum-=a[left];
-            left++;
-
-        }
         if(sum == k)
         {
-            start = left;
-            end = right;
             maxlen = max(maxlen,right-left+1);
+            right++;
+            if(right<n)
+            {
+                sum += a[right];
+            }
         }
-        right++;
-        if(right<n)
+        else if(sum <k)
         {
-            sum += a[right];
+            right++;
+            if(right<n)
+            {
+                sum+=a[right];
+            }
         }
+        else{
+            sum -= a[left];
+            left++;
+        }
+        
     }
-
-    for (int i = start; i <= end; i++)
-    {
-        cout << a[start] << " ";
-    }
-    cout << endl;
     return maxlen;
 
+    
+
+    
 }
 
 int main()
